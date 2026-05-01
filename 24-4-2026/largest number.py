582eb543a07eb8b17d63e01fec20f26a82a682ef
grishabhimani-create <grishabhimani@gmail.com>
2026-05-01 13:25:27 +0530

    Add files via upload

---
+++ 24-4-2026/largest number.py
@@ -0,0 +1,11 @@
+# find largest number of list
+
+number = list ( map, int,input("Enter numbers:"))
+
+largest = number[0] # is index from list
+
+for num in number :
+    if num>largest:
+        largest = num
+        print("Largest:" , lergest)
+
